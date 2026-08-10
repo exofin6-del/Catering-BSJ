9c70e488f712f35c20dc87e5797e69d4a2338ad1
aditya <herlambangaditya27@gmail.com>
2026-08-11 01:56:36 +0700

    Revert "feat: add admin@gmail.com seed account"
    This reverts commit 9994c12076281757a9d287c3e4eb6bafd2de3023.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,13 +20,14 @@ class DatabaseSeeder extends Seeder
             PackageSeeder::class,
         ]);
 
-        User::query()->firstOrCreate(
-            ['email' => 'admin@gmail.com'],
-            [
-                'name' => 'Admin',
-                'password' => Hash::make('cateringbsj'),
-                'email_verified_at' => now(),
-            ],
-        );
+        // User::factory(10)->create();
+        // User::query()->firstOrCreate(
+        //     ['email' => 'admin@example.com'],
+        //     [
+        //         'name' => 'Example Admin',
+        //         'password' => Hash::make('12345678'),
+        //         'email_verified_at' => now(),
+        //     ],
+        // );
     }
 }
