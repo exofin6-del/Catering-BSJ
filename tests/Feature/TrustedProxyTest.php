8e8cd1cb5f414a5f4316561b8868467c03b1a6d2
aditya <herlambangaditya27@gmail.com>
2026-08-12 01:35:14 +0700

    Fix HTTPS redirects behind Railway proxy

---
+++ tests/Feature/TrustedProxyTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class TrustedProxyTest extends TestCase
+{
+    public function test_redirects_use_the_forwarded_https_scheme(): void
+    {
+        $response = $this->get('/kategori', [
+            'X-Forwarded-Proto' => 'https',
+        ]);
+
+        $response->assertRedirect();
+        $this->assertStringStartsWith(
+            'https://',
+            (string) $response->headers->get('Location'),
+        );
+    }
+}
